56c252409d86fcd0ea90c2021b5e990e8ddac74e
Carter Green <cartergr@usc.edu>
2019-07-21 20:19:31 -0500

    Initial working version

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,44 @@
-pub fn truncate_path(path: &str, len: usize, delimiter: char) -> String {
-    let mut truncated = "".to_owned();
-    // if path.get(0..1) == Some("~") {
-    //     truncated.push_str("~/");
-    // } else {
-    //     truncated.push('/');
-    // }
-    for dir in path.split('/') {
-        match dir.get(0..len) {
-            Some(truncated_dir) => truncated.push_str(truncated_dir),
-            None => truncated.push_str(dir),
-        };
-        truncated.push(delimiter);
+extern crate dirs;
+
+pub fn replace_home(path: &str) -> String {
+    let home_dir_path = dirs::home_dir();
+    if home_dir_path.is_none() {
+        return path.to_owned();
     }
-    return truncated;
+    let home_dir_path_uw = home_dir_path.unwrap();
+    let home_dir = home_dir_path_uw.to_str().unwrap_or("");
+    return match path.get(..home_dir.len()) {
+        Some(sub_path) if sub_path == home_dir => format!("~{}", path.get(home_dir.len()..path.len()).unwrap_or("")),
+        _ => path.to_owned(),
+    };
+}
+
+pub fn truncate_path(path: &str, target_len: usize, trunc_len: usize) -> String {
+    if path.len() <= target_len {
+        return path.to_owned();
+    }
+    let mut out = "".to_owned();
+    let mut out_len = path.len();
+    let components = path.split('/').collect::<Vec<&str>>();
+    for (idx, dir) in components.iter().enumerate() {
+        // Never truncate current directory
+        if idx == components.len() - 1 {
+            out.push_str(dir);
+        } else if out_len <= target_len || dir.len() <= trunc_len {
+            out.push_str(dir);
+            out.push('/');
+        } else {
+            match dir.get(..trunc_len) {
+                Some(t_dir) => {
+                    out.push_str(t_dir);
+                    out_len -= dir.len() - trunc_len;
+                },
+                None => out.push_str(dir),
+            };
+            out.push('/');
+        }
+    }
+    return out;
 }
 
 #[cfg(test)]
@@ -20,7 +46,38 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_replace_home() {
+        assert_eq!(replace_home(&format!("{}/git/vinoteca/vinoteca/node_modules", dirs::home_dir().unwrap().to_str().unwrap_or(""))),
+                   "~/git/vinoteca/vinoteca/node_modules");
+    }
+
+    #[test]
+    fn test_replace_home_outside_home() {
+        assert_eq!(replace_home("/usr/lib/llvm/include"), "/usr/lib/llvm/include");
+    }
+
+    #[test]
     fn test_truncated_path() {
-        assert_eq!(truncate_path("/usr/lib/llvm/include", 1, '/'), "/u/l/l/include");
+        assert_eq!(truncate_path("/usr/lib/llvm/include", 1, 1), "/u/l/l/include");
+    }
+
+    #[test]
+    fn test_root() {
+        assert_eq!(truncate_path("/", 1, 1), "/");
+    }
+
+    #[test]
+    fn test_home() {
+        assert_eq!(truncate_path("~", 1, 1), "~");
+    }
+
+    #[test]
+    fn test_truncate_some_but_not_others() {
+        assert_eq!(truncate_path("/usr/lib/llvm/include", 18, 1), "/u/l/llvm/include");
+    }
+
+    #[test]
+    fn test_different_trunc_len() {
+        assert_eq!(truncate_path("~/git/vinoteca/vinoteca/node_modules", 1, 2), "~/gi/vi/vi/node_modules");
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::env;
+
+use pshort;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let target_len: usize = args.get(2).unwrap_or(&"1".to_owned()).parse::<usize>().unwrap();
+    let path = pshort::replace_home(args.get(1).unwrap_or(&"~".to_owned()));
+    println!("{}", pshort::truncate_path(&path, target_len, 1));
 }
